c4857b03eb9dfdf094af266886d86c5da5cd05c1
Sorohpsoe <matteo.devergne@gmail.com>
2023-12-21 09:49:48 +0100

    texte

--- Golang/main.go
+++ Golang/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	// Ouvrir le fichier en lecture
+	file, err := os.Open("../texte.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Créer un scanner pour lire le fichier ligne par ligne
+	scanner := bufio.NewScanner(file)
+
+	// Lire chaque ligne du fichier
+	for scanner.Scan() {
+		line := scanner.Text() // Obtenir la ligne courante
+		fmt.Println(line)      // Afficher la ligne
+	}
+
+	// Vérifier les erreurs lors de la lecture
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }

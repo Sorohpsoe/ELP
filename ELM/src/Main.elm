090b0a2d4a7d9b9bcba3eed066929a8d5d86e506
Sorohpsoe <matteo.devergne@gmail.com>
2024-01-25 17:06:59 +0100

    Refactor code and add comments

--- ELM/src/Main.elm
+++ ELM/src/Main.elm
@@ -20,6 +20,7 @@ main =
     }
 
 -- Model
+--Creation of models
 
 type alias Model =
   { state : State
@@ -27,16 +28,18 @@ type alias Model =
   , listDef : (List Word)
   , guess : String}
 
+--The three different states the Model can be
 type State
   = Failure
   | Loading
   | Success 
 
+--Contain the word and its definition
 type alias Word = 
   { word : String
   , meanings : (List Meaning)}
 
-
+--Contain the type of the word in partofSpeech and the different definitions
 type alias Meaning =
   { partOfSpeech : String
   , definitions : (List Definition)}
@@ -51,7 +54,7 @@ init _ =
 -- Update
 
 type Msg 
-  = MorePlease
+  = AnotherWord
   | GotWord (Result Http.Error String)
   | GotIndex Int
   | GotDef (Result Http.Error (List Word))
@@ -60,68 +63,85 @@ type Msg
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
-  case msg of 
-    MorePlease ->
+  case msg of
+
+    -- The user clicked on the button to get another word, put the state to Loading and get the list of words.
+    AnotherWord ->
       ({ model |state =Loading ,wordList =[], listDef = [], guess = "" }, getListWord)
+    
+    -- The list of words is got, put the state to Success and get a random index.
     GotWord result ->
       case result of
         Ok words ->
           ( { model |state =Success, wordList = String.words words, listDef = [], guess = "" }, Random.generate GotIndex (Random.int 1 1000) )
         Err _ ->
           ({ model |state =Failure ,wordList = [],listDef = [], guess = "" }, Cmd.none)
+
+    -- The random index generated, get the word from the list of words and get its definition.
     GotIndex index ->
       case (getWordFromIndex model.wordList index ) of 
         "Error" -> ({ model |state =Failure ,wordList = [],listDef = [], guess = "" }, Cmd.none)
         word -> ( { model |state =Success, wordList = model.wordList, listDef = [], guess = "" }, getRandomDef word )
+
+    -- The definition of the word is got, put the state to Success and display the word and its definition.
     GotDef result ->
       case result of 
         Ok def ->
           ({ model |state =Success ,wordList = model.wordList,listDef = def, guess = "" }, Cmd.none)
         Err _ ->
           ({ model |state =Failure ,wordList = model.wordList,listDef = [], guess = "" }, Cmd.none)
+    
+    -- The user typed a new guess, update the model with the new guess.
     NewGuess try ->
       ({model | state=model.state,wordList = model.wordList,listDef=model.listDef, guess =try}, Cmd.none)
+
+    -- The user clicked on the button to show the answer, update the model with the answer.
     GiveAnswer ->
-      ({model | state = model.state,wordList = model.wordList,listDef=model.listDef,guess = leMot (model.listDef)},Cmd.none)
+      ({model | state = model.state,wordList = model.wordList,listDef=model.listDef,guess = chosenWord (model.listDef)},Cmd.none)
 
 
 -- View 
 
 view : Model -> Html Msg
-view model = 
-  div[] 
-  [ h1 [] [text (if String.toLower (model.guess) ==String.toLower(leMot( model.listDef)) then leMot(model.listDef) else "Guess the word !")]
+-- Show the header text, change backgound to lightgreen if the player got the word right.
+view model =
+  div [style "background-color" (if String.toLower (model.guess) == String.toLower (chosenWord (model.listDef)) then "lightgreen" else "white"), style "height" "100vh", style "width" "100vw"]
+  [ h1 [] [text (if String.toLower (model.guess) == String.toLower (chosenWord (model.listDef)) then "You found it !" else "Guess the word !")]
   , viewWord model
   ]
 
 viewWord : Model -> Html Msg
 viewWord model = 
+  -- Differents cases if the .txt file is loaded or not, if it is loading and loaded.
   case model.state of 
     Failure ->
       div[]
         [ text "I could not load a word"
-        , button [onClick MorePlease][text "Reload"]]
+        , button [onClick AnotherWord][text "Reload"]]
     Loading ->
       text "Loading ..."
     Success ->
+      -- Add the input section and buttons, all are linked to their functions. Show if the word is guessed right or wrong.
       ul[]
-        [ wordsList model.listDef  
-        ,h3[][text "Type here : ", input [value model.guess, onInput NewGuess][],div[][text (if String.toLower(model.guess) == String.toLower(leMot (model.listDef)) then "Bien joué" else (if model.guess == "" then "" else "Mauvaise réponse"))]
-         ], button [ onClick MorePlease, style "display" "block" ][text "Play Again !"], button [onClick GiveAnswer, style "display" "block"][text "Show Answer"]]
+        [h3[][text "Type here : ", input [value model.guess, onInput NewGuess][],div[][text (if String.toLower(model.guess) == String.toLower(chosenWord (model.listDef)) then "Bien joué" else (if model.guess == "" then "" else "Mauvaise réponse"))]
+         ], button [ onClick AnotherWord, style "display" "block" ][text "Play Again !"], button [onClick GiveAnswer, style "display" "block"][text "Show Answer"],
+          wordsList model.listDef]
 
-  
+--Get the output of the word with the API with all the definitions related to it  
 getRandomDef : String -> Cmd Msg
 getRandomDef word = 
   Http.get 
     { url = "https://api.dictionaryapi.dev/api/v2/entries/en/" ++ word
     , expect = Http.expectJson GotDef listMotsDecoder}
 
+--Get the list of words 
 getListWord : Cmd Msg 
 getListWord = 
   Http.get
-    { url = "http://localhost:8000/words.txt"
+    { url = "http://localhost:8000/src/words.txt"
     , expect = Http.expectString GotWord}
 
+--Take a word in a list of words at a specified index
 getWordFromIndex : List String -> Int -> String
 getWordFromIndex list index = 
   case list of 
@@ -131,34 +151,40 @@ getWordFromIndex list index =
         0 -> h
         _ -> getWordFromIndex t (index-1)
 
--- Fonctions 
+-- Functions which returns Html Msg
+
+-- Display the definitions of the word
 defList : (List Definition) -> Int -> Html Msg
 defList definitions i = 
   case definitions of 
     [] -> div[][]
     (h::t) -> div [] [ul [] [ul[][text (String.fromInt(i)++ ". " ++ h.def)]], defList t (i+1)]
     
+-- Display the different meanings of the word (defList)
 definitionList : (List Meaning)-> Html Msg 
 definitionList meanings = 
   case meanings of  
     [] -> div[][]
     (h::t) -> div[] [h3  [][ul [][text (h.partOfSpeech ++ " :")]], defList h.definitions 0,definitionList t ]
 
+-- Display the word and its definition (definitionList)
 wordsList : (List Word) -> Html Msg
 wordsList words =
   case words of
     [] -> div[][]
     (h::t) -> div[][h2[][text "Meaning :" ],  definitionList h.meanings , wordsList t]
 
-leMot : (List Word) -> String
-leMot words = 
+--Put the chosen word in a string format
+chosenWord : (List Word) -> String
+chosenWord words = 
   case words of 
     [] -> "Probleme"
     h::_ -> h.word 
 
 
 
---Decoder 
+-- Decoder 
+-- Turn the json given by the API into types created above.
 
 wordsDecoder : Decoder Word
 wordsDecoder =
